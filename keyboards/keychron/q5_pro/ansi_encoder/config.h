ba54000e87806cfcd0edcca60f947b1c849c12cd
arpan3t <zvbierman@gmail.com>
2025-04-03 19:38:00 -0500

    Added comment layout to keymap

--- keyboards/keychron/q5_pro/ansi_encoder/config.h
+++ keyboards/keychron/q5_pro/ansi_encoder/config.h
@@ -14,6 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #pragma once
 
 #ifdef RGB_MATRIX_ENABLE
